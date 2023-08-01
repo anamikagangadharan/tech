e32e3222d1609a6e38f3b187b7ffb3312337c213
alhad-tm <alhadchikkutm@gmail.com>
2023-08-01 16:50:19 +0530

    header

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header/Header';
 
+
 function App() {
   return (
     <div className="App">
-      Tech
       <Header/>
     </div>
   );
