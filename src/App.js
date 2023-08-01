f6a30ef016795823bc74dd79e38f53c110b0b119
alhad-tm <alhadchikkutm@gmail.com>
2023-08-01 18:09:27 +0530

    working on contact section

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header/Header';
+import Main from './components/Main/Main';
+import About from './components/About/About';
 
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <Main/>
+      <About/>
     </div>
   );
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -26,7 +26,7 @@ const Header = () => {
       ) : (
             <ul className={css.rightlist}>
               
-                   <li><Link to="about" onClick={()=>setMenuOpened(false)} spy={true} smooth={true}>Home</Link></li>
+                   <li><Link to="home" onClick={()=>setMenuOpened(false)} spy={true} smooth={true}>Home</Link></li>
                 <li><Link to="about" onClick={()=>setMenuOpened(false)} spy={true} smooth={true}>About</Link></li>
                 <li><Link to="services" onClick={()=>setMenuOpened(false)} spy={true} smooth={true}>Services</Link></li>
                 <li><Link to="contact" onClick={()=>setMenuOpened(false)} spy={true} smooth={true}>Contact Us</Link></li>
