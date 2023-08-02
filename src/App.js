0f277680fad9587ee64981b61d2bc2936e0c6eba
alhad-tm <alhadchikkutm@gmail.com>
2023-08-02 11:29:06 +0530

    service section

--- src/App.js
+++ src/App.js
@@ -3,17 +3,23 @@ import './App.css';
 import Header from './components/Header/Header';
 import Main from './components/Main/Main';
 import About from './components/About/About';
+import Services from './components/Services/Services';
+
 
 
 function App() {
+ 
   return (
     <div className="App">
+      
       <Header/>
       <Main/>
       <About/>
+      <Services/>
+    
     </div>
   );
 }
 
 export default App;
- 
+    
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,6 +6,9 @@ import { Link } from 'react-scroll'
 const Header = () => {
   const mobile = window.innerWidth <= 768 ? true : false;
   const [menuOpened, setMenuOpened] = useState(false); 
+
+
+ 
   return (
     <div className={css.container}>
         <div className={css.left}>
