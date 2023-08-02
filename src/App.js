351629980230bee1896fae14bb472d36a118fa54
alhad-tm <alhadchikkutm@gmail.com>
2023-08-02 12:56:17 +0530

    working on footer

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Header from './components/Header/Header';
 import Main from './components/Main/Main';
 import About from './components/About/About';
 import Services from './components/Services/Services';
+import Subscribe  from './components/Subscribe/Subscribe';
+import Footer from './components/Footer/Footer';
 
 
 
@@ -16,6 +18,8 @@ function App() {
       <Main/>
       <About/>
       <Services/>
+      <Subscribe/>
+      <Footer/> 
     
     </div>
   );
