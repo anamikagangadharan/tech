e3ffeca0d391e0dd79b8e852d1faf0591a5f610c
alhad-tm <alhadchikkutm@gmail.com>
2023-08-03 16:41:42 +0530

    overall layout

--- src/components/Subscribe/Subscribe.jsx
+++ src/components/Subscribe/Subscribe.jsx
@@ -9,9 +9,9 @@
             <span>Newsletter</span>
 
             <span>Subscribe to our sales newsletter to stay up to date with our latest products & services</span>
-
-            <input className={css.inpsubscribe} type="text" placeholder='Email Address' />
             <div className={css.wrap}>
+            <input className={css.inpsubscribe} type="text" placeholder='Email Address' />
+           
             <button className={css.subscribe}>Subscribe</button>
             </div>
            
