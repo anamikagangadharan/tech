622e862e6920b3b370c76ecb0fb37e59bcc357f0
alhad-tm <alhadchikkutm@gmail.com>
2023-08-04 16:34:54 +0530

    changes done

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import css from "./Header.module.css"
 import Logo1 from "../../assets/invicious-tech-logo.svg"
 import Bars from "../../assets/bars.svg"
+import Close from "../../assets/close.png"
 import { Link } from 'react-scroll'
 const Header = () => {
   const mobile = window.innerWidth <= 768 ? true : false;
@@ -39,10 +40,13 @@ const Header = () => {
       ) : (
             <ul className={css.rightlist} onClick={()=>setMenuOpened(false)}>
               
+              {mobile?  <img className={css.close} src={Close} alt="" /> :""   }      
                    <li><Link to="home" onClick={()=>setMenuOpened(false)} spy={true} smooth={true}>Home</Link></li>
                 <li><Link to="about" onClick={()=>setMenuOpened(false)} spy={true} smooth={true}>About</Link></li>
                 <li><Link to="services" onClick={()=>setMenuOpened(false)} spy={true} smooth={true}>Services</Link></li>
                 <li><Link to="contact" onClick={()=>setMenuOpened(false)} spy={true} smooth={true}>Contact Us</Link></li>
+
+        
               
             </ul>)}
             {/* </div> */}
