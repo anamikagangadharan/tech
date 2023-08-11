63c0b74a40eb208266b2c07e666ad9c33672f56e
alhad-tm <alhadchikkutm@gmail.com>
2023-08-11 16:10:05 +0530

    added emailjs, toastify

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,7 +1,32 @@
-import React from 'react'
+import React,{useRef} from 'react'
 import css from "./Contact.module.css"
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import emailjs from "@emailjs/browser";
 
 const Contact = () => {
+  const notify = () => toast.success("Sent Successfully");
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_uonholy", 
+      "template_y03ilxe",
+      form.current,
+      "0ShbDNI1kk0Ln2HDZ"
+      )  
+      .then(
+        (result) => { 
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text); 
+        }
+      );
+  };
   return (
     <div className={css.container}>
       
@@ -14,54 +39,55 @@ const Contact = () => {
 for your success in the digital world.</span>
       </div>
 
-      <form className={css.div3}>
+      <form className={css.div3}  ref={form} onSubmit={sendEmail}>
         <div className={css.set}>
             <div className={css.line}>
             <label htmlFor="">Full name *</label>
-            <input className={css.contactinp}  required type="text" name="" id=""  placeholder='Enter Your Name'/>
+            <input className={css.contactinp}  required type="text" name="to_name" id=""  placeholder='Enter Your Name'/>
             </div>
             <div className={css.line}>
             <label htmlFor="">Your Email *</label>
-            <input className={css.contactinp}  required  type="email" name="" id="" placeholder='Enter Your Email' />
+            <input className={css.contactinp}  required  type="email" name="to_email" id="" placeholder='Enter Your Email' />
             </div>
         </div>
         <div className={css.set}>
             <div className={css.line}>
             <label htmlFor="">Phone number *</label>
-            <input className={css.contactinp}  required type="number" name="" id=""  placeholder='Enter Your Number'/>
+            <input className={css.contactinp}  required type="number" name="number" id=""  placeholder='Enter Your Number'/>
             </div>
             <div className={css.line}>
             <label htmlFor="">Company *</label>
-            <input className={css.contactinp}  required type="text" name="" id=""  placeholder='Enter Your Company name'/>
+            <input className={css.contactinp}  required type="text" name="company" id=""  placeholder='Enter Your Company name'/>
             </div>
         </div>
         <div className={css.set}>
             <div className={css.line}>
             <label for="cars">Subject *</label>
 
-            <select className={css.selectinput} name="cars" id="cars">
-  <option className={css.opt} value="volvo">How can we help ?</option>
-  <option className={css.opt} value="saab">IT Consultancy Service</option>
-  <option className={css.opt} value="mercedes">Web Design & Development</option>
-  <option className={css.opt} value="mercedes">Mobile App Development</option>
-  <option className={css.opt} value="mercedes">Digital Branding Services</option>
-  <option className={css.opt} value="mercedes">Software Development</option>
+            <select className={css.selectinput} name="subject" id="cars">
+  <option className={css.opt} value="Nil">How can we help ?</option>
+  <option className={css.opt} value="Consultancy service">IT Consultancy Service</option>
+  <option className={css.opt} value="Web Design & Development">Web Design & Development</option>
+  <option className={css.opt} value="Mobile App Development">Mobile App Development</option>
+  <option className={css.opt} value="digital Branding Services">Digital Branding Services</option>
+  <option className={css.opt} value="Software Development">Software Development</option>
 
 </select>
           
             </div>
             <div className={css.line}>
             <label htmlFor="">Country *</label>
-            <input className={css.contactinp}  required type="text" name="" id=""  placeholder='Enter Your Company name'/>
+            <input className={css.contactinp}  required type="text" name="country" id=""  placeholder='Enter Your Country'/>
             </div>
         </div>
 
         <div className={css.lastset}>
             <label htmlFor="">Message</label>
            
-          <input className={css.messageinp} type="text"  placeholder='Enter some message here'/>
+          <input className={css.messageinp} type="text" name='message' placeholder='Enter some message here'/>
         </div>
-        <button className={css.subbtn}>Submit</button>
+        <button onClick={notify} className={css.subbtn}>Submit</button>
+        <ToastContainer style={{width:"257px", height:"50px"}} autoClose={600} position={"bottom-center"} />
       </form>
 
          
